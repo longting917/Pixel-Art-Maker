3e5e701adb3af050fb11ae899ea79dce196a3832
longting917 <longyan1121998@gmail.com>
2020-04-18 10:37:56 -0600

    Remove click color on/off from design.js
    Had conversation with Kathleen W on Udacity. She said the color on/off function was causing the issue. Since it was not a requirement of the original project, I've omitted it and cleaned up the code a little bit.

--- designs.js
+++ designs.js
@@ -1,5 +1,6 @@
-function makeGrid() {
+const table = document.getElementById('pixelCanvas');
 
+function makeGrid() {
 
 	const inputHeight = document.getElementById('inputHeight');
 	const inputWidth = document.getElementById('inputWidth');
@@ -9,51 +10,42 @@ function makeGrid() {
 
 	for(i = 1; i <= rows; i++) {
 
-		var table = document.getElementById('pixelCanvas');
 		table.appendChild(document.createElement('TR'));
 		var lastRow = table.lastChild;
 
 		for(j = 1; j <= cols; j++) {
+
 			lastRow.appendChild(document.createElement('TD'));
 			lastRow.lastChild.setAttribute('class','cell');
 		}
 	}
 
-	
-
-	document.querySelector('#pixelCanvas').addEventListener('click', function (evt) {
-	    if (evt.target.nodeName === 'TD') {  // ← verifies target is desired element
+	table.addEventListener('click', function (evt) {
+	    if (evt.target.nodeName === 'TD') {
 	        var color = document.getElementById('colorPicker').value;
-	        if (evt.target.hasAttribute("style") === true){
-	        	evt.target.removeAttribute("style");
-	        } else {
-		        console.log(color);
+		        // Tests below to debug why color might not show up on a click. 
+		        // Below checks passed 2018-04-18
+		        // console.log(color);
+		        // console.log(evt.target.hasAttribute("style"));
+		        // console.log('A cell was clicked');
 		        evt.target.style.backgroundColor = color;
-		        console.log(evt.target.hasAttribute("style"));
-		        console.log('A cell was clicked');
-	        }
-
 	   	}
 	});
-
 }
 
 document.addEventListener('DOMContentLoaded', function () {
 
 	document.querySelector('#sizePicker').addEventListener('click', function (evt) {
 	
-
-
-		if(document.getElementById('pixelCanvas').hasChildNodes() === true){
-			var tbl = document.getElementById('pixelCanvas');
-			while(tbl.rows.length > 0) {
-				  tbl.deleteRow(0);
+		if(table.hasChildNodes() === true){
+			// Removes old rows so they don't just keep appending on the bottom
+			// deleting the table.
+			while(table.rows.length > 0) {
+				  table.deleteRow(0);
 			}
 		}
-
-		if (evt.target.getAttribute("type")==="submit") {  // ← verifies target is desired element
+		if (evt.target.getAttribute("type")==="submit") {
 			evt.preventDefault();	
-
 			makeGrid(evt)
 		}
 	});
